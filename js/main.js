08a97972d01a8760a8c5cd79790632bb59053777
Sam Billingham <sambillie.sb@gmail.com>
2017-12-30 17:18:46 +0000

    add data from request

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 steem.api.getAccounts(['dan', 'ned'], function(err, response){
   response.forEach( function(user){
-      console.log('IMAGE')
+      var jsonData = user.json_metadata ? JSON.parse(user.json_metadata).profile : {}
+
+      console.log("IMAGE", jsonData.profile_image ? 'https://steemitimages.com/2048x512/' + jsonData.profile_image : '')
       console.log(user.name);
       console.log('REPUTATION');
       console.log('STEEM POWER');
-      console.log('STEEM');
-      console.log('STEEM DOLLARS');
-      console.log('Followers');
-      console.log('Following');
-      console.log('NUM OF POSTS');
+      console.log('STEEM', user.balance);
+      console.log('STEEM DOLLARS', user.sbd_balance);
+
+      steem.api.getFollowCount( user.name , function(err, user) {
+        console.log("Followers", user.follower_count);
+        console.log("Following", user.following_count);
+      });
+      console.log("NUM OF POSTS", user.post_count);
+
   });
 });
