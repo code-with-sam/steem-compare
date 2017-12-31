b64ee1130bd9fe99ae63ffb20abaae07a38e27b4
Sam Billingham <sambillie.sb@gmail.com>
2017-12-31 13:05:26 +0000

    displaying/searching for data

--- js/main.js
+++ js/main.js
@@ -4,31 +4,71 @@ var vestingShares,
     totalVestingShares,
     totalVestingFundSteem = null;
 
+$('.grid').on('click', '.remove-user', function(){
+  $(this).parent().remove();
+});
 
+$('.search-btn').on('click', function(){
+  let data = $('.search').val();
+  let users = data.split(',').map(function(user) {
+    return user.trim();
+  });
+  // console.log(users);
+  addUsers(users)
+});
+
+function addUsers(users){
+  getAccounts(users).then(function(data){
+
+      displayAccounts(proccessData(data));
+  });
+}
 
 getGlobalProps()
   .then(function(){
-    return getAccounts(['dan', 'ned'])
+    return getAccounts(['ned', 'dan', 'blocktrades'])
   })
   .then(function(data){
-    console.log(data)
-    console.log(proccessData(data));
 
-    displayAccounts(proccessData(data));
+    proccessData(data).then((data) => {
+      console.log(data)
+      displayAccounts(data);
+
+    })
+    // console.log(data)
+    // console.log(proccessData(data));
+
+    // displayAccounts(proccessData(data));
   })
+
 
 function displayAccounts(accounts){
   var $grid = $('.grid');
 
   accounts.forEach(function(user){
-    console.log(user);
+    // console.log(user);
     var template =
-      `<div class="col-md-4">
-      <h2>${user.name}</h2>
-      <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-      <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      `<div class="col-md-3 col-sm-4">
+      <img src="${user.image}" class="rounded-circle" height="100px" width="100px">
+      <h2>${user.name} <span class="badge badge-secondary">${user.rep}</span></h2>
+      <h3>STEAM POWER: ${user.sp}</h3>
+      <h3>STEAM POWER: ${user.sp} + ${user.delegatedSpIn} - ${user.delegatedSpOut}</h3>
+
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped" role="progressbar" style="width: ${user.vp}%;" aria-valuenow="${user.vp}" aria-valuemin="0" aria-valuemax="100">${user.vp}% </div>
+      </div>
+
+      <h4>STEEM BALANCE: ${user.steem}</h4>
+      <h4>SBD Balance: ${user.sbd}</h4>
+      <h4>POSTS: ${user.numOfPosts}</h4>
+
+      <h4>FollowerCount: ${user.followerCount}</h4>
+      <h4>FollowingCount: ${user.followingCount}</h4>
+      <h4>💵 USD: ${user.usdValue}</h4>
+
+      <button type="button" class="btn btn-dark remove-user">Remove</button>
       </div>`;
-      console.log(template)
+      // console.log(template)
       $grid.append(template);
   })
 
@@ -51,6 +91,12 @@ function getAccounts(accountNames){
 function proccessData(accounts){
 
   var accountsData = [];
+
+  let processAllData = new Promise((resolve, reject) => {
+  // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+  // In this example, we use setTimeout(...) to simulate async code.
+  // In reality, you will probably be using something like XHR or an HTML5 API.
+
 
   accounts.forEach( function(user){
     // store meta Data
@@ -80,6 +126,7 @@ function proccessData(accounts){
     //     usdValue = value;
     //   });
     // })
+
     accountsData.push({
       name: user.name,
       image: jsonData.profile_image ? 'https://steemitimages.com/2048x512/' + jsonData.profile_image : '',
@@ -97,5 +144,35 @@ function proccessData(accounts){
     });
   });
 
-  return accountsData;
+  var promises = [];
+  // accountsData.forEach() => { }
+  for (var i = 0; i < accountsData.length; i++) {
+    promises.push( steem.api.getFollowCount( accountsData[i].name ))
+  }
+  Promise.all(promises)
+    .then((data) => {
+        for (var i = 0; i < data.length; i++) {
+          accountsData[i].followerCount = data[i].follower_count
+          accountsData[i].followingCount = data[i].following_count
+        }
+    })
+
+  var usdValues = [];
+
+  for (var i = 0; i < accountsData.length; i++) {
+    usdValues.push(steem.formatter.estimateAccountValue( accounts[i]))
+  }
+
+  Promise.all(usdValues)
+    .then((data) => {
+        for (var i = 0; i < data.length; i++) {
+          accountsData[i].usdValue = data[i]
+        }
+        resolve(accountsData); // return promise
+    })
+
+  });
+
+  return processAllData;
+
 }
